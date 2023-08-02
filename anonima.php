e4d00db4e8095fb5600c0cd71d4b7eb3f1e301ca
luizvelasco <luizvelasco@gmail.com>
2023-08-02 08:01:07 -0300

    anonima

---
+++ anonima.php
@@ -0,0 +1,9 @@
+<?php
+require_once "src/Classes/Fornecedor.php";
+
+ $fornecedor = new Fornecedor;
+
+$fornecedor->autorizar(new Class {
+    public $nome = "joão";
+    public $senha = "123456";
+});
--- src/Classes/Fornecedor.php
+++ src/Classes/Fornecedor.php
@@ -6,4 +6,13 @@ class Fornecedor
     public string $razaoSocial;
     public string $nomeFantasia;
     public int $cnpj;
+
+    function autorizar($usuario)
+    {
+        if ($usuario->nome == "joão" && $usuario->senha == "123456"){
+            echo "<br>Autorizado";
+        } else {
+            echo "<br>Bloqueado";
+        }
+    }
 }
