3d2bcc1e48790638550f9f85a1ffdae0ce2569fa
Luiz Velasco <luizvelasco@gmail.com>
2023-08-07 19:47:57 -0300

    cap4

--- namespace/declaracao.php
+++ namespace/declaracao.php
@@ -3,7 +3,11 @@
 require_once "../src/Email/Envio.php";
 require_once "../src/SMS/Envio.php";
 
-$email = new \Email\Envio;
-$sms = new \SMS\Envio;
+$email = new \App\Email\Envio;
+$sms = new \App\SMS\Envio;
 
-var_dump($email, $sms);
+echo \App\Email\VERSAO . "<hr>";
+
+\App\Email\validar("luizvelasco@gmail.com");
+
+echo "<hr>";
--- src/Email/Envio.php
+++ src/Email/Envio.php
@@ -1,8 +1,21 @@
 <?php
 
-namespace Email;
+namespace App\Email;
+
+CONST VERSAO = 1.0;
 
 class Envio
 {
-    
+    public function enviar(): void
+    {
+        $adaptador = new Adaptadores\Mailgun\Adaptador;
+        $transporte = new Transporte;
+
+        var_dump($adaptador, $transporte);
+    }    
+}
+
+function validar(string $email): void
+{
+    echo "O endereço $email é valido";
 }
